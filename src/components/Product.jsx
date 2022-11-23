0ea3305542fc577eeb85cf5565ae2cf2cf916f31
sabi1918 <117071260+sabi1918@users.noreply.github.com>
2022-11-23 13:49:53 +0400

    fsdgfsdf

---
+++ src/components/Product.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import Price from "./Price"
+import ProductName from "./ProductName"
+
+
+
+const Product = () => {
+    return (
+        <>
+        <ProductName/>
+        <Price/>
+        </>
+    )
+}
+
+export default Product
---
+++ src/components/ProductName.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProductName = () => {
+    return (
+        <div>
+            <h3>Sabina Nahmetova</h3>
+        </div>
+    )
+}
+
+export default ProductName
